39d15d67e884fdc39c63f9f9b3f6c9c22ff4c732
Enzo Gerino <gerinoenzo@gmail.com>
2022-10-31 17:56:37 -0300

    up cors

--- src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
+++ src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-//@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = "https://demofrontend-b30f4.web.app")
 public class PersonaController {
     @Autowired
