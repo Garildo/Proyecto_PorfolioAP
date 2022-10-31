8b17274128bfd9396773c7c46ffccd2d8eb518bb
Enzo Gerino <gerinoenzo@gmail.com>
2022-10-31 00:09:51 -0300

    test

--- src/main/java/com/PorfolioAplicacion/demo/Security/MainSecurity.java
+++ src/main/java/com/PorfolioAplicacion/demo/Security/MainSecurity.java
@@ -7,6 +7,8 @@ package com.PorfolioAplicacion.demo.Security;
 import com.PorfolioAplicacion.demo.Security.Service.UserDetailsImpl;
 import com.PorfolioAplicacion.demo.Security.jwt.JwtEntryPoint;
 import com.PorfolioAplicacion.demo.Security.jwt.JwtTokenFilter;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,11 +22,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  *
  * @author BANGHO/Egerino
  */
+
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -45,6 +49,29 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+
+        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
+        List<String> list2 = Arrays.asList(new String[]{"https://demofrontend-b30f4.web.app´"});
+        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
+        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(list1);
+        corsConfiguration.setAllowedOrigins(list2);
+        corsConfiguration.setAllowedMethods(list3);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(list4);
+
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().configurationSource(request -> corsConfiguration);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+    /*protected void configure(HttpSecurity http) throws Exception {
         http.cors()
                 .and()
                 .csrf()
@@ -56,7 +83,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
                 .authenticated()
                 .and().exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
+    }*/
 
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
