f15d0f94044c9f0f60b02a42e2b35fe924f4d054
Garildo <90223678+Garildo@users.noreply.github.com>
2023-02-22 12:38:37 -0300

    modificacion controller explaboral

--- src/main/java/com/PorfolioAplicacion/demo/Controller/CExperienciaLaboral.java
+++ src/main/java/com/PorfolioAplicacion/demo/Controller/CExperienciaLaboral.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.PorfolioAplicacion.demo.Controller;
 
 import com.PorfolioAplicacion.demo.Dto.DtoExperienciaLaboral;
@@ -29,7 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = {"http://localhost:4200"})
+//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://demofrontend-b30f4.web.app") 
 public class CExperienciaLaboral {
     @Autowired
     SExperienciaLaboral sExperienciaLaboral;
