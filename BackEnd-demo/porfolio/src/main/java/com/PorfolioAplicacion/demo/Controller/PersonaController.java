e549ad30744f3b89d960095490c7b12e472c2c4f
Enzo Gerino <gerinoenzo@gmail.com>
2023-02-22 20:31:35 -0300

    ulitmos cambios

--- BackEnd-demo/porfolio/src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
+++ BackEnd-demo/porfolio/src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
@@ -12,10 +12,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/personas")
@@ -50,17 +52,17 @@ public class PersonaController {
         return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     }*/
     
-    /*@PostMapping("/create")
+    @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoPersona dtopersona){
         if(StringUtils.isBlank(dtopersona.getNombre()))
             return new ResponseEntity(new Mensaje("El nombre es obligatorio"), HttpStatus.BAD_REQUEST);
-        if(personaService.existsByNombre(dtoedu.getNombre()))
+        if(personaService.existsByNombre(dtopersona.getNombre()))
             return new ResponseEntity(new Mensaje("Esa Persona ya existe"), HttpStatus.BAD_REQUEST);
-        Persona persona = new Persona(dtopersona.getNombre(), dtopersona.getDescripcion());
+        Persona persona = new Persona(dtopersona.getNombre(), dtopersona.getDescripcion(), dtopersona.getApellido(), dtopersona.getImg());
         personaService.save(persona);
         
         return new ResponseEntity(new Mensaje("Persona Agregada"), HttpStatus.OK);
-    }*/
+    }
     
     @PutMapping("/update/{id}")
     public ResponseEntity<?> update(@PathVariable("id")int id, @RequestBody dtoPersona dtopersona){
@@ -86,5 +88,7 @@ public class PersonaController {
         personaService.save(persona);
         return new ResponseEntity(new Mensaje("Persona Actualizada"), HttpStatus.OK);
     }
+    
+    
     
 }
