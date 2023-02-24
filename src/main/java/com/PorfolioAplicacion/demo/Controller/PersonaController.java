fa496f3b2b6141bf2557cf48b2b8efaa90c02213
Garildo <90223678+Garildo@users.noreply.github.com>
2023-02-24 00:06:52 -0300

    UpdatePersona

--- src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
+++ src/main/java/com/PorfolioAplicacion/demo/Controller/PersonaController.java
@@ -19,8 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://demofrontend-b30f4.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "https://demofrontend-b30f4.web.app")
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
